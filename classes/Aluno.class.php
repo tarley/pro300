9c035f434ec83b058151ecb55f20474ef53118bd
CarinaRoriz <carinaroriz@hotmail.com>
2016-10-11 22:22:37 -0300

    Criação das classes do projeto

---
+++ classes/Aluno.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Carina
+ * Date: 11/10/2016
+ * Time: 20:53
+ */
+class Aluno
+{
+    /**
+     * Insere os dados de um aluno a partir de um array
+     */
+    function Insert($array){
+
+    }
+
+    /**
+     * Atualiza os dados de um aluno a partir de um array
+     */
+    function Update($array){
+
+    }
+
+    /**
+     * Retorna os dados de um aluno a partir do seu codigo
+     */
+    function getDados($codAluno){
+
+    }
+
+    /**
+     * Retorna uma lista de todos os alunos
+     */
+    function getLista(){
+
+    }
+
+    /**
+     * Retorna uma lista de todos os alunos que participam de determinada atividade
+     */
+    function getListaByAtividade($codAtividade){
+
+    }
+}
---
+++ classes/Atividade.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Carina
+ * Date: 11/10/2016
+ * Time: 21:01
+ */
+class Atividade
+{
+    /**
+     * Insere os dados de uma atividade a partir de um array
+     */
+    function Insert($array){
+
+    }
+
+    /**
+     * Atualiza os dados de uma atividade a partir de um array
+     */
+    function Update($array){
+
+    }
+
+    /**
+     * Encerra uma atividade a partir do seu codigo
+     */
+    function EncerrarAtividade($codAtividade){
+
+    }
+
+    /**
+     * Retorna os dados de uma atividade a partir do seu codigo
+     */
+    function getDados($codAtividade){
+
+    }
+
+    /**
+     * Retorna uma lista de todos as atividades
+     */
+    function getLista(){
+
+    }
+
+    /**
+     * Retorna uma lista de todos as atividades por professor
+     */
+    function getListaByProfessor($codProfessor)
+    {
+
+    }
+
+    /**
+     * Inscreve um aluno em uma atividade a partir de seus codigos
+     */
+    function InscricaoAvaliacao($codAtividade, $codAluno){
+
+    }
+}
