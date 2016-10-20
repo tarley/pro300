0c19b0fd8e33e85a37d0196db522f24da4dd6ad9
Carina <carinaroriz@hotmail.com>
2016-10-20 19:32:37 -0200

    Classe Inscrição

---
+++ classes/Incricao.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Carina
+ * Date: 11/10/2016
+ * Time: 21:01
+ */
+class Inscricao
+{
+    /**
+     * Insere os dados de uma inscrição a partir de um array
+     */
+    function Insert($array){
+
+    }
+
+    /**
+     * Encerra uma inscrição a partir do seu codigo
+     */
+    function EncerrarInscricao($codInscricao){
+
+    }
+
+    /**
+     * Retorna os dados de uma inscrição a partir do seu codigo
+     */
+    function getDados($codInscricao){
+
+    }
+}
