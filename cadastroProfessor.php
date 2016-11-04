8fc03e43b1c314c1fadddbbf8c21d8db5899ef49
fillipeperpetuo <fillipeperpetuo@gmail.com>
2016-11-04 09:37:21 -0200

    Alterções na pagina de cadastro de professor

--- cadastroProfessor.php
+++ cadastroProfessor.php
@@ -1,6 +1,6 @@
 <?php
-	$permissaoPagina = "P"; // A ou P de acordo com o perfil do usuário
-	require_once ("controleAcesso.php");
+$permissaoPagina = "P"; // A ou P de acordo com o perfil do usuário
+require_once ("controleAcesso.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,48 +33,48 @@
 							<div class="x_content">
 
 								<!-- Início do conteudo da página  -->
-								<form class="form-horizontal form-label-left" novalidate>
+								<form class="form-horizontal form-label-left" method="POST" novalidate>
 									<br />
 									<div class="item form-group">
 										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Nome <span class="required">*</span>
 										</label>
 										<div class="col-md-8 col-sm-6 col-xs-12">
-											<input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="name" placeholder="Nome" required="required" type="text">
+											<input id="nome" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="name" placeholder="Nome" required="required" type="text">
 										</div>
 									</div>
 									<div class="item form-group">
 										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">E-mail <span class="required">*</span>
 										</label>
 										<div class="col-md-8 col-sm-6 col-xs-12">
-											<input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="email" placeholder="E-mail" required="required" type="text">
+											<input id="email" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="email" placeholder="E-mail" required="required" type="text">
 										</div>
 									</div>
 									<div class="item form-group">
-										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Matéria <span class="required">*</span>
+										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Telefone <span class="required">*</span>
 										</label>
 										<div class="col-md-6 col-sm-6 col-xs-12">
-											<input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="matéria" placeholder="Matéria" required="required" type="text">
+											<input id="telefone" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="telefone" placeholder="Telefone" required="required" type="text">
 										</div>
 									</div>
 									<div class="item form-group">
 										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Senha <span class="required">*</span>
 										</label>
 										<div class="col-md-6 col-sm-6 col-xs-12">
-											<input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="senha" placeholder="Senha" required="required" type="password">
+											<input id="senha" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="senha" placeholder="Senha" required="required" type="password">
 										</div>
 									</div>
 									<div class="item form-group">
 										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Confirmar Senha <span class="required">*</span>
 										</label>
 										<div class="col-md-6 col-sm-6 col-xs-12">
-											<input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="confirmarsenha" placeholder="Confirmar Senha" required="required" type="password">
+											<input id="confirmarSenha" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="confirmarsenha" placeholder="Confirmar Senha" required="required" type="password">
 										</div>
 									</div>
 									<div class="ln_solid"></div>
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-3">
-											<button type="submit" class="btn btn-primary">Cancelar</button>
-											<button id="send" type="submit" class="btn btn-success">Salvar</button>
+											<button id="cancelar" type="submit" class="btn btn-primary">Cancelar</button>
+											<button id="salvar" name="salvar" type="submit" class="btn btn-success">Salvar</button>
 										</div>
 									</div>
 								</form>
@@ -100,4 +100,65 @@
 </div>
 <?php include_once ("includes/script.php"); ?>
 </body>
+
+<script>
+	$("#salvar").click(function (e) {
+		var nome = $("#nome").val();
+		var email = $("#email").val();
+		var telefone = $("#telefone").val();
+		var senha = $("#senha").val();
+		var confirmarSenha = $("#confirmarSenha").val();
+		var msg = "";
+
+		if (nome == "") {
+			msg += "Preencha o campo nome\n\r";
+		}
+
+		if (email == "") {
+			msg += "Preencha o campo e-mail\n\r";
+		}
+
+		if (telefone == "") {
+			msg += "Preencha o campo telefone\n\r";
+		}
+
+		if (senha == "") {
+			msg += "Preencha o campo senha\n\r";
+		}
+
+		if (confirmarSenha == "") {
+			msg += "Preencha o campo Confirmar senha\n\r";
+		}
+
+		if (msg != "") {
+			alert("Preencha os campos: \n\r" + msg);
+			e.preventDefault();
+			return;
+		}
+
+		if(senha != confirmarSenha){
+			alert("Campo Senha e Confirmar Senha devem ser iguais.");
+			e.preventDefault();
+			return;
+		}
+
+
+
+		$.post("classes/Professor.class.php?acao=inserir", { nome: nome, email: email, telefone: telefone , senha: senha })
+			.done(function(result){
+				alert(result.msg);
+
+				if(!result.erro){
+					$("#nome").val("");
+					$("#email").val("");
+					$("#telefone").val("");
+					$("#senha").val("");
+					$("#confirmarSenha").val("");
+				}
+			});
+
+		e.preventDefault();
+	});
+
+</script>
 </html>
