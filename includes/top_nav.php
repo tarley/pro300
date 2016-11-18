665572428e69dcc3a89f9824f031ac9c89e14329
katernets <aternets@gmail.com>
2016-11-18 21:08:51 -0200

    Update
    Removendo imagem.

--- includes/top_nav.php
+++ includes/top_nav.php
@@ -13,7 +13,7 @@
 			<ul class="nav navbar-nav navbar-right">
 				<li class="">
 					<a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-						<img src="images/img.jpg" alt=""><?php echo $nome; ?>
+						<img src="" alt=""><?php echo $nome; ?>
 						<span class=" fa fa-angle-down"></span>
 					</a>
 					<ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
