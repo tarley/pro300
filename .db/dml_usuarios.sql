c24ea7cf0dd67bc1753ab7ba42c3d5b7601101ca
RodrigoRoliveira <rodrigorodrigueso1@hotmail.com>
2016-11-18 20:42:25 -0200

    Update dml_usuarios.sql

--- .db/dml_usuarios.sql
+++ .db/dml_usuarios.sql
@@ -1,20 +1,77 @@
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'albert@newtopaiva.br','teste123','0000','98765-4321','aluno','Albert Rocha');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'felipe@newtopaiva.br','teste123','0000','98765-4321','aluno','Felipe');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'tarley@newtopaiva.br','teste123','0000','98765-4321','professores','Tarley');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'poliana@newtopaiva.br','teste123','0000','98765-4321','aluno','Poliana'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'allan@newtopaiva.br','teste123','0000','98765-4321','professores','Allan Silva Ferreira');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'gustavo.eduardo@newtopaiva.br','teste123','0000','98765-4321','aluno','Gustavo Eduardo'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'andre@newtopaiva.br','teste123','0000','98765-4321','aluno','Andre carvalho');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'poliana@newtopaiva.br','teste123','0000','98765-4321','aluno','Poliana');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'carina@newtopaiva.br','teste123','0000','98765-4321','aluno','Carina Roriz Pimenta');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'joao.heringer@newtopaiva.br','teste123','0000','98765-4321','aluno','João Carlos Heringer');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'carlos.henrique@newtopaiva.br','teste123','0000','98765-4321','aluno','Carlos Henrique'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'vivian@newtopaiva.br','teste123','0000','98765-4321','aluno','Vivian');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'felipe.perpetuo@newtopaiva.br','teste123','0000','98765-4321','aluno','Felipe Perpétuo'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'ater@newtopaiva.br','teste123','0000','98765-4321','aluno','Ater Nets'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'igor.henrique@newtopaiva.br','teste123','0000','98765-4321','aluno','Igor Henrique'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'joh@newtopaiva.br','teste123','0000','98765-4321','aluno','Joh'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'vitor.alessandro@newtopaiva.br','teste123','0000','98765-4321','aluno','Vitor alessandro');
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'juliano@newtopaiva.br','teste123','0000','98765-4321','aluno','Juliano'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'bruna@newtopaiva.br','teste123','0000','98765-4321','aluno','Bruna'); 
-insert into tb_usuario (cod_usuario, email, senha, ra, telefone, perfil, nome) values (null,'gustavo.rezende@newtopaiva.br','teste123','0000','98765-4321','aluno','Gustavo Rezende');
+--=======
+--Usuario
+--=======
+
+INSERT INTO tb_usuario (`email`, `senha`, `ra`, `nome`, `telefone`, `perfil`)
+VALUES ('rodrigorodrigues@gmail.com', '123', '222', 'Rodrigo', '3155', 'P');
+
+INSERT INTO tb_usuario (`email`, `senha`, `ra`, `nome`, `telefone`, `perfil`)
+VALUES ('katernetz@gmail.com', '123', '111', 'Ater', '3155', 'P');
+
+INSERT INTO tb_usuario (`email`, `senha`, `ra`, `nome`, `telefone`, `perfil`)
+VALUES ('brunomonteiro@gmail.com', '123', '333', 'Bruno', '3155', 'P');
+
+INSERT INTO tb_usuario (`email`, `senha`, `ra`, `nome`, `telefone`, `perfil`)
+VALUES ('manuelsilva@gmail.com', '123', '444', 'Manoel', '3155', 'A');
+
+INSERT INTO tb_usuario (`email`, `senha`, `ra`, `nome`, `telefone`, `perfil`)
+VALUES ('carloshenrique@gmail.com', '123', '555', 'Carlos', '3155', 'A')
+
+--====================
+--Criptografar a senha
+--====================
+
+UPDATE tb_usuario set senha = sha1(123)
+WHERE cod_usuario = 1;
+
+UPDATE tb_usuario set senha = sha1(123)
+WHERE cod_usuario = 2;
+
+UPDATE tb_usuario set senha = sha1(123)
+WHERE cod_usuario = 3;
+
+UPDATE tb_usuario set senha = sha1(123)
+WHERE cod_usuario = 4;
+
+UPDATE tb_usuario set senha = sha1(123)
+WHERE cod_usuario = 5
+
+--===========
+--Atividades
+--===========
+
+INSERT INTO tb_atividade (`desc_atividade`, `token`, `data_inicio`, `data_fim`, `data_encerramento_atv`, `cod_professor`) 
+VALUES ('Álgebra Linear', '456', '2016/1/10', '2016/1/11', NULL, '1'); 
+
+INSERT INTO tb_atividade (`desc_atividade`, `token`, `data_inicio`, `data_fim`, `data_encerramento_atv`, `cod_professor`) 
+VALUES ('Probabilidade', '433', '2016/5/12', '2016/5/13', NULL, '1'); 
+
+INSERT INTO tb_atividade (`desc_atividade`, `token`, `data_inicio`, `data_fim`, `data_encerramento_atv`, `cod_professor`) 
+VALUES ('Instalações Hidráulicas', '422', '2016/6/15', '2016/6/16', NULL, '1'); 
+
+INSERT INTO tb_atividade (`desc_atividade`, `token`, `data_inicio`, `data_fim`, `data_encerramento_atv`, `cod_professor`) 
+VALUES ('Cálculo Vetorial', '566', '2016/3/11', '2016/3/12', 'NuLL', '2'); 
+
+INSERT INTO tb_atividade (`desc_atividade`, `token`, `data_inicio`, `data_fim`, `data_encerramento_atv`, `cod_professor`) 
+VALUES ('Estatística', '454', '2016/2/11', '2016/2/12', 'NULL', '3') 
+
+--=========
+--Inscrição
+--=========
+
+INSERT INTO tb_inscricao (`p1`, `p300`, `nota_final`, `acrescimo`, `grupo`, `data_inscricao`, `lider`, `situacao`, `cod_aluno`, `cod_atividade`) 
+VALUES ('21', NULL, NULL, NULL, 1, '2016/10/25', '1', '1', '4', '1'); 
+
+INSERT INTO tb_inscricao (`p1`, `p300`, `nota_final`, `acrescimo`, `grupo`, `data_inscricao`, `lider`, `situacao`, `cod_aluno`, `cod_atividade`) 
+VALUES ('15', NULL, NULL, NULL, 1, '2016/09/15', '0', '1', '5', '1');
+
+INSERT INTO tb_inscricao (`p1`, `p300`, `nota_final`, `acrescimo`, `grupo`, `data_inscricao`, `lider`, `situacao`, `cod_aluno`, `cod_atividade`) 
+VALUES ('11', NULL, NULL, NULL, 1, '2016/09/20', '0', '1', '5', '1');
+
+INSERT INTO tb_inscricao (`p1`, `p300`, `nota_final`, `acrescimo`, `grupo`, `data_inscricao`, `lider`, `situacao`, `cod_aluno`, `cod_atividade`) 
+VALUES ('17', NULL, NULL, NULL, 1, '2016/10/02', '0', '1', '5', '1');
+
+INSERT INTO tb_inscricao (`p1`, `p300`, `nota_final`, `acrescimo`, `grupo`, `data_inscricao`, `lider`, `situacao`, `cod_aluno`, `cod_atividade`) 
+VALUES ('12', NULL, NULL, NULL, 1, '2016/10/03', '0', '1', '5', '1')
+
+
