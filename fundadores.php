21f3faf83557fa14386b905b7b14ce31d0d27f2a
igorsoaresassis <igorsoaresassis@hotmail.com>
2016-11-22 12:48:02 -0200

    Add files via upload
    inicio fundadores

---
+++ fundadores.php
@@ -0,0 +1,51 @@
+<?php
+	$permissaoPagina = "P";
+	require_once ("controleAcesso.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include_once ("includes/header.php"); ?>
+</head>
+
+<body class="nav-md">
+	<div class="container body">
+		<div class="main_container">
+			<?php include_once ("includes/left_col.php"); ?>
+			<?php include_once ("includes/top_nav.php"); ?>
+
+			<!-- Page Content Start -->
+			<div class="right_col" role="main">
+				<div class="">
+
+					<div class="page-title">
+						<div class="title_left">
+							<h3>Fundadores</h3>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							 
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+			<!-- Page Content End -->
+
+		</div>
+	</div>
+	<div id="custom_notifications" class="custom-notifications dsp_none">
+		<ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+		</ul>
+		<div class="clearfix"></div>
+		<div id="notif-group" class="tabbed_notifications"></div>
+	</div>
+	<?php include_once ("includes/script.php"); ?>
+
+</body>
+</html>
+
