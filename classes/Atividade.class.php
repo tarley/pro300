9feb3005ff5376cea48e5ae3fcfeb7e4ba9af188
CarinaRoriz <carinaroriz@hotmail.com>
2016-11-26 21:49:48 -0200

    - Correção BUG da tela Administrar Atividades - Correção formato da data na tela de Cadastro de Atividades

--- classes/Atividade.class.php
+++ classes/Atividade.class.php
@@ -257,8 +257,8 @@ class Atividade
             SELECT  cod_atividade,
                     desc_atividade,
                     token,
-                    date_format(data_inicio, \'%m/%d/%Y\') as data_inicio,
-                    date_format(data_fim,\'%m/%d/%Y\') as data_fim,
+                    date_format(data_inicio, \'%d/%m/%Y\') as data_inicio,
+                    date_format(data_fim,\'%d/%m/%Y\') as data_fim,
                     data_encerramento_atv,
                     cod_professor
             FROM tb_atividade a 
