dd5b265cef52521dbe6cc68c9ff8c0953841bb5b
igorsoaresassis <igorsoaresassis@hotmail.com>
2016-11-27 18:12:44 -0200

    Add files via upload
    Fundadores

--- fundadores.php
+++ fundadores.php
@@ -3,10 +3,24 @@
 	require_once ("controleAcesso.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
+	
+    <meta https-equiv="X-UA-Compatible" content="IE=edge">
+   	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Pro 300</title>
+    
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+    
+    <link href="css/fundadores.css" rel="stylesheet">
 	<?php include_once ("includes/header.php"); ?>
+
+
 </head>
 
 <body class="nav-md">
@@ -27,7 +41,332 @@
 					
 					<div class="row">
 						<div class="col-md-12 col-sm-12 col-xs-12">
-							 
+							<div class="container container-fundador">
+								<div class="row">
+									<div class="col-lg-12 text-center">
+										<h3 class="section-subheading text-muted">Professores Respons&aacute;veis</h3>
+									</div>
+									<div class="col-sm-3"></div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/Monica_Machado.png" class="img-circle img-fundadores" alt="">
+											<h4>M&ocirc;nica Machado</h4>
+											<p class="text-muted">Coordenadora / Prof(a). Orientadora</p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/tarley_lana.png" class="img-circle img-fundadores" alt="">
+											<h4>Tarley Lana</h4>
+											<p class="text-muted">Prof. Orientador</p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3"></div>
+
+            						<div class="col-lg-12 text-center">
+										<h3 class="section-subheading text-muted">Sistemas de Informa&ccedil;&atilde;o - 6&#186 Per&iacute;odo</h3>
+									</div>
+
+            						<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/alex.png" class="img-circle img-fundadores" alt="">
+											<h4>Alex</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">                     
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/anderson.png" class="img-circle img-fundadores" alt="">
+											<h4>Anderson</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">                     
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/andre_carvalho.png" class="img-circle img-fundadores" alt="">
+											<h4>Andr&eacute; Santos Carvalho</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://br.linkedin.com/in/andrescarvalho"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/Ater.png" class="img-circle img-fundadores" alt="">
+											<h4>Ater Souza</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://br.linkedin.com/in/ater-souza-53712922"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/bruna_frazoni.png" class="img-circle img-fundadores" alt="">
+											<h4>Bruna Frazoni</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://facebook.com/bruninha.abalen?fref=nf"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/bruno_sartori.png" class="img-circle img-fundadores" height="239px" alt="">
+											<h4>Bruno Sartori</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://www.linkedin.com/in/bruno-sartori-40b9b4116?trk=hp-identity-name "><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+										   <img src="images/img_fundador/carina_roriz.png" class="img-circle img-fundadores" alt="">
+											<h4>Carina Roriz Pimenta</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://facebook.com/carina.rorizpimenta"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://www.linkedin.com/in/carina-roriz-pimenta-a7b349a3?trk=hp-identity-name"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/Carlos_Henrique.png" class="img-circle img-fundadores" alt="">
+											<h4>Carlos Henrique</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/fillipe_perpetuo.png" class="img-circle img-fundadores" alt="">
+											<h4>Fillipe Perp&eacute;tuo</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://facebook.com/fillipeperpetuo"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://linkedin.com/in/fillipe-perpétuo-639b0075?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/francisco.jpg" class="img-circle img-fundadores" alt="">
+											<h4>Francisco Coelho</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://www.facebook.com/francisco.augusto.31"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/gustavo_eduardo.jpg" class="img-circle img-fundadores" alt="">
+											<h4>Gustavo Eduardo</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://www.facebook.com/francisco.augusto.31"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/gustavo.jpg" class="img-circle img-fundadores" alt="">
+											<h4>Gustavo</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://www.facebook.com/francisco.augusto.31"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/Igor_Henrique.png" class="img-circle img-fundadores" alt="">
+											<h4>Igor Henrique</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+										   <img src="images/img_fundador/joao_heringer.png" class="img-circle img-fundadores" alt="">
+											<h4>Jo&atilde;o Carlos Heringer</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/johnny_junio.png" class="img-circle img-fundadores" alt="">
+											<h4>Johnny Ramos</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://www.facebook.com/johnny.junio1"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://www.linkedin.com/in/johnny-j%C3%BAnio-7a3194120?trk=hp-identity-name"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/lucas_amaral.png" class="img-circle img-fundadores" alt="">
+											<h4>Lucas Amaral</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="https://facebook.com/lucas.amaral8"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://br.linkedin.com/in/lucas-amaral-a6403772"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/Napoliana.png" class="img-circle img-fundadores" alt="">
+											<h4>Napoliana Santos Lima</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+										   <img src="images/img_fundador/otavio.png" class="img-circle img-fundadores" alt="">
+											<h4>Ot&aacute;vio Batista</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+                						<div class="team-member">
+											<img src="images/img_fundador/pedro_henrique.png" class="img-circle img-fundadores" alt="">
+											<h4>Pedro Henrique</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="https://www.linkedin.com/in/pedro-henrique-6601a2115"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/rodrigo_rodrigues.png" class="img-circle img-fundadores" alt="">
+											<h4>Rodrigo Rodrigues</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank" href="https://www.linkedin.com/in/rodrigo-rodrigues-2495a155?trk=nav_responsive_tab_profile_pic"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/vitor_alessandro.png" class="img-circle img-fundadores" alt="">
+											<h4>Vitor Alessandro</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+									<div class="col-sm-3">
+										<div class="team-member">
+											<img src="images/img_fundador/vivian_silva.png" class="img-circle img-fundadores" alt="">
+											<h4>Vivian Silva</h4>
+											<p class="text-muted"></p>
+											<ul class="list-inline social-buttons">
+												<li><a target="_blank"  href="#"><i class="fa fa-facebook"></i></a>
+												</li>
+												<li><a target="_blank"  href="#"><i class="fa fa-linkedin"></i></a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 
