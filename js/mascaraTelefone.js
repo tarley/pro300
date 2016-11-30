2c28e7bede0e3adc13feb528f9894efd6fc681ea
Vivian Henrique Silva <vivian.sistemas@yahoo.com.br>
2016-11-30 02:17:36 -0200

    Update mascaraTelefone.js

--- js/mascaraTelefone.js
+++ js/mascaraTelefone.js
@@ -11,9 +11,13 @@ function execmascara(){
 	v_obj.value=v_fun(v_obj.value)
 }
 function mtel(v){
-	v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
-	v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
-	v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+	v=v.replace(/\D/g,"");             
+	v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); 
+	v=v.replace(/(\d)(\d{4})$/,"$1-$2");    
 	return v;
 }
 
+function soDigitos(v){
+	v=v.replace(/\D/g,""); 
+	return v;
+} 
