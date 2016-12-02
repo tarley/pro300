705ee8b482d2af5deab7d8ffdf616ceaddfc04d5
fillipeperpetuo <fillipeperpetuo@gmail.com>
2016-12-02 21:42:44 -0200

    Alteração dos dados professor (Bug mensagem)

--- alterarProfessor.php
+++ alterarProfessor.php
@@ -1,159 +1,200 @@
 <?php
 $permissaoPagina = "P"; // A ou P de acordo com o perfil do usuário
-require_once ("controleAcesso.php");
+require_once("controleAcesso.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<?php include_once ("includes/header.php"); ?>
+    <?php include_once("includes/header.php"); ?>
 </head>
 
 <body class="nav-md">
 <div class="container body">
-	<div class="main_container">
-		<?php include_once ("includes/left_col.php"); ?>
-		<?php include_once ("includes/top_nav.php"); ?>
+    <div class="main_container">
+        <?php include_once("includes/left_col.php"); ?>
+        <?php include_once("includes/top_nav.php"); ?>
 
-		<!-- Page Content Start -->
-		<div class="right_col" role="main">
-			<div class="">
+        <!-- Page Content Start -->
+        <div class="right_col" role="main">
+            <div class="">
 
-				<div class="page-title">
-					<div class="title_left">
-						<h3>Editar Dados Cadastrais</h3><!-- Título da Página -->
-					</div>
-				</div>
+                <div class="page-title">
+                    <div class="title_left">
+                        <h3>Editar Dados Cadastrais</h3>
+                    </div>
+                </div>
 
-				<div class="clearfix"></div>
-				<div class="row">
-					<div class="col-md-12 col-sm-12 col-xs-12">
+            </div>
+            <div class="clearfix"></div>
 
-						<div class="x_panel">
-							<div class="x_content">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
 
-								<!-- Início do conteudo da página  -->
-								<form class="form-horizontal form-label-left" method="POST" novalidate>
-									<br />
-									<div class="item form-group">
-										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
-											<label>Nome*:</label>
-											<input id="nome" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="name" placeholder="Nome" required="required" type="text">
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
-											<label>E-mail:</label>
-											<input id="email" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="email" placeholder="E-mail" required="required" type="text">
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
-											<label>Telefone:</label>
-											<input id="telefone" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="telefone" placeholder="Telefone" required="required" type="text">
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
-											<label>Senha:</label>
-											<input id="senha" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="senha" placeholder="Senha" required="required" type="password">
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
-											<label>Confimar Senha:</label>
-											<input id="confirmarSenha" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="confirmarsenha" placeholder="Confirmar Senha" required="required" type="password">
-										</div>
-									</div>
-									<div class="ln_solid"></div>
-									<div class="form-group">
-										<div class="col-md-6 col-md-offset-3">
-											<button id="cancelar" type="submit" class="btn btn-primary">Cancelar</button>
-											<button id="salvar" name="salvar" type="submit" class="btn btn-success">Salvar</button>
-										</div>
-									</div>
-								</form>
-								<!-- Fim do conteudo da página  -->
+                        <div class="x_content">
 
-							</div>
-						</div>
-					</div>
-				</div>
+                            <form id="formEditar" class="form-horizontal form-label-left" method="POST" novalidate>
 
-			</div>
+                                <div id="resultado">
+                                    <div class="item form-group">
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
+                                                <label>Nome*:</label>
+                                                <input type="text" name="nome" id="nome" class="form-control"
+                                                       oninput="javascript:retirarAcento(this);">
+                                            </div>
+                                        </div>
 
-		</div>
-		<!-- Page Content End -->
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
+                                                <label>Telefone*:</label>
+                                                <input type="text" name="telefone" id="telefone" class="form-control"
+                                                       onkeyup="mascara( this, mtel );" maxlength="15">
+                                            </div>
+                                        </div>
 
-	</div>
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
+                                                <label>E-mail*:</label>
+                                                <input type="text" name="emailCadastro" id="emailCadastro"
+                                                       class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
+                                            <p>* Informações Obrigatórias</p>
+                                        </div>
+                                        <br/>
+
+                                        <div class="ln_solid"></div>
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-3">
+                                                <button type="submit" id="btnCancelar" class="btn btn-primary">
+                                                    Cancelar
+                                                </button>
+                                                <button id="btnEnviar" type="submit" class="btn btn-success">Enviar
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- Page Content End -->
 </div>
 <div id="custom_notifications" class="custom-notifications dsp_none">
-	<ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
-	</ul>
-	<div class="clearfix"></div>
-	<div id="notif-group" class="tabbed_notifications"></div>
+    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+    </ul>
+    <div class="clearfix"></div>
+    <div id="notif-group" class="tabbed_notifications"></div>
 </div>
-<?php include_once ("includes/script.php"); ?>
-</body>
+<?php include("includes/script.php"); ?>
+
 
 <script>
-	$("#salvar").click(function (e) {
-		var nome = $("#nome").val();
-		var email = $("#email").val();
-		var telefone = $("#telefone").val();
-		var senha = $("#senha").val();
-		var confirmarSenha = $("#confirmarSenha").val();
-		var msg = "";
-
-		if (nome == "") {
-			msg += "Preencha o campo nome\n\r";
-		}
-
-		if (email == "") {
-			msg += "Preencha o campo e-mail\n\r";
-		}
-
-		if (telefone == "") {
-			msg += "Preencha o campo telefone\n\r";
-		}
-
-		if (senha == "") {
-			msg += "Preencha o campo senha\n\r";
-		}
-
-		if (confirmarSenha == "") {
-			msg += "Preencha o campo Confirmar senha\n\r";
-		}
-
-		if (msg != "") {
-			alert("Preencha os campos: \n\r" + msg);
-			e.preventDefault();
-			return;
-		}
-
-		if(senha != confirmarSenha){
-			alert("Campo Senha e Confirmar Senha devem ser iguais.");
-			e.preventDefault();
-			return;
-		}
+    $.ajax({
+        url: 'classes/Professor.class.php?acao=buscarPorId'
+    }).done(function (data) {
+        if (data.erro) {
+            alert(data.msg);
+            window.location = "login.php";
+        } else {
+            $('#nome').val(data[0].nome);
+            $('#telefone').val(data[0].telefone);
+            $('#emailCadastro').val(data[0].email);
+        }
+    });
 
 
+$("#btnEnviar").click(function (e) {
+        var nome = $("#nome").val();
+        var telefone = $("#telefone").val();
+        var emailCadastro = $("#emailCadastro").val();
 
-		$.post("classes/Professor.class.php?acao=inserir", { nome: nome, email: email, telefone: telefone , senha: senha })
-			.done(function(result){
+        if (nome == "" || nome.length < 10) {
+            alert("Preencha o campo Nome com pelo menos 10 caracteres!");
+            e.preventDefault();
+            return;
+        }
+
+        if (telefone != "" && telefone.length < 15) {
+            alert("Preencha o campo Telefone no formato (00)00000-0000");
+            e.preventDefault();
+            return;
+        }
+
+        if (emailCadastro == "") {
+            alert("Preencha o campo Email!");
+            e.preventDefault();
+            return;
+        }
+
+        $.post("classes/Professor.class.php?acao=alterar", {
+            nome: nome,
+            telefone: telefone,
+            emailCadastro: emailCadastro
+        })
+ 
+           .done(function(result){
 				alert(result.msg);
 
-				if(!result.erro){
-					$("#nome").val("");
-					$("#email").val("");
-					$("#telefone").val("");
-					$("#senha").val("");
-					$("#confirmarSenha").val("");
-				}
-			});
+                if (result.erro) {
 
-		e.preventDefault();
-	});
+                } else {
+                    window.location = "administrarAtividades.php";
+                }
+
+            });
+
+        e.preventDefault();
+    });
+
+    $("#btnCancelar").click(function (e) {
+        window.location = "administrarAtividades.php";
+        e.preventDefault();
+    });
+
+    function retirarAcento(objResp) {
+        var varString = new String(objResp.value);
+        var stringAcentos = new String('üÜ1234567890\\!@#$%¨&*()-_+={[}]:;.,?|<>/´\"~^\`\'°');
+        var stringSemAcento = new String('uU');
+
+        var i = new Number();
+        var j = new Number();
+        var cString = new String();
+        var varRes = '';
+
+        for (i = 0; i < varString.length; i++) {
+            cString = varString.substring(i, i + 1);
+            for (j = 0; j < stringAcentos.length; j++) {
+                if (stringAcentos.substring(j, j + 1) == cString) {
+                    cString = stringSemAcento.substring(j, j + 1);
+                }
+            }
+            varRes += cString;
+        }
+        objResp.value = varRes;
+    }
+
 
 </script>
+<script src="js/mascaraTelefone.js"></script>
+
+</body>
 </html>
+
+
+
+
+
+
+
+
