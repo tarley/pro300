8ad168b69270096aa99253e9222c5501d5fb3e48
pedraoalmeida <pedro.cec@hotmail.com>
2016-12-03 21:13:44 -0200

    Add files via upload

--- alterarSenha.php
+++ alterarSenha.php
@@ -33,34 +33,34 @@ require_once ("controleAcesso.php");
 						<div class="x_panel">
 							<div class="x_content">
 
-								<form class="form-horizontal form-label-left" novalidate>
+								<form class="form-horizontal form-label-left" method="POST" novalidate>
 
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
 											<label>Senha Atual*:</label>
-											<input type="password" name="senha" class="form-control">
+											<input type="password" id="senha" name="senha" class="form-control" maxlength="10">
 										</div>
 									</div>
 
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
 											<label>Nova Senha*:</label>
-											<input type="password" name="novaSenha" class="form-control">
+											<input type="password" id="novaSenha" name="novaSenha" class="form-control" maxlength="10">
 										</div>
 									</div>
 
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-3 col-sm-6 col-xs-12">
 											<label>Confirmação da Senha*:</label>
-											<input type="password" name="confSenha" class="form-control">
+											<input type="password" id="confSenha" name="confSenha" class="form-control" maxlength="10">
 										</div>
 									</div>
 
 									<div class="ln_solid"></div>
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-3">
-											<button type="submit" class="btn btn-primary">Cancelar</button>
-											<button id="send" type="submit" class="btn btn-success">Enviar</button>
+											<button type="button" id= "btnCancelar" class="btn btn-primary">Cancelar</button>
+                                            <button type="button" id="btnEnviar" class="btn btn-success">Enviar</button>
 										</div>
 									</div>
 								</form>
@@ -84,5 +84,75 @@ require_once ("controleAcesso.php");
 	<div id="notif-group" class="tabbed_notifications"></div>
 </div>
 <?php include_once ("includes/script.php"); ?>
+
+<script>
+
+    $("#btnEnviar").click(function (e) {
+        var senhaAtual = $("#senha").val();
+        var senha = $("#novaSenha").val();
+        var confirmarSenha = $("#confSenha").val();
+
+        if (senhaAtual == "") {
+            alert("Preencha o campo senha atual!");
+            e.preventDefault();
+            return;
+        }
+
+        if (senha == "") {
+            alert("Preencha o campo nova senha!");
+            e.preventDefault();
+            return;
+        }
+
+        if (confirmarSenha == "") {
+            alert("Preencha o campo Confirmar senha!");
+            e.preventDefault();
+            return;
+        }
+
+        if (senha != confirmarSenha) {
+            alert("Digite senhas iguais!");
+            e.preventDefault();
+            return;
+        }
+
+        if (senha.length < 6) {
+            alert("A senha deve conter entre 6 e 10 caracteres!");
+            e.preventDefault();
+            return;
+        }
+
+        if (senhaAtual == senha) {
+            alert("A nova senha é igual a senha atual!");
+            e.preventDefault();
+            return;
+        }
+
+        $.post("classes/Aluno.class.php?acao=alterarSenha", {
+            senhaAtual: senhaAtual,
+            novaSenha: senha
+        }).done(function (result) {
+            console.log(result);
+            alert(result.msg);
+
+            if (result.erro) {
+
+            } else {
+                window.location = "administrarAtividades.php";
+            }
+
+        });
+
+        e.preventDefault();
+
+    });
+
+    $("#btnCancelar").click(function (e) {
+        window.location = "administrarAtividades.php";
+        e.preventDefault();
+    });
+
+</script>
+
 </body>
 </html>
