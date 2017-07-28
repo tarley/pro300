ae2e5c7985166296f3b88920907f127992824dc1
Tarley <tarley.lana@gmail.com>
2017-07-28 00:13:33 +0000

    Refatorando DialogService

--- app/services/DialogService.js
+++ app/services/DialogService.js
@@ -1,25 +1,25 @@
 function DialogService() {
 
-    var displayLength = 4000;
+    this.displayLength = 4000;
 
-    return {
-        showMessage: function(message, params = []) {
-            var template = jQuery.validator.format(message);
-            Materialize.toast(template(params), displayLength);
-        },
-        showResponse: function(response) {
-            if (response.data.mensagem)
-                this.showMessage(response.data.mensagem);
-            else if (response.data)
-                console.log(response.data);
-            else
-                console.log(response);
-        },
-        showError: function(response) {
-            if(response.status == '401')
-                console.log("Usuário não possui permissão para acessar o recurso.");
-            else
-                this.showMessage("{0} - {1}", [response.status, response.statusText]);
-        }
-    };
+    this.showMessage = function(message, params = []) {
+        var template = jQuery.validator.format(message);
+        Materialize.toast(template(params), this.displayLength);
+    }
+    
+    this.showResponse = function(response) {
+        if (response.data.mensagem)
+            this.showMessage(response.data.mensagem);
+        else if (response.data)
+            console.log(response.data);
+        else
+            console.log(response);
+    }
+    
+    this.showError = function(response) {
+        if (response.status == '401')
+            console.log("Usuário não possui permissão para acessar o recurso.");
+        else
+            this.showMessage("{0} - {1}", [response.status, response.statusText]);
+    }
 }
