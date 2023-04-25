0b778f3ee492433cbc5d682d67f8071694228e0c
tarley <tarley.lana@gmail.com>
2023-04-24 21:47:29 -0300

    fix: corrige a conta de e-mail
    Por estar em um domínio diferente, o sistema não estava enviando e-mail para recuperar a senha.

--- Config.php
+++ Config.php
@@ -11,16 +11,23 @@
     define('DB_PASSWORD', $DB_PASSWORD);
     
     /*CONFIGURAÇÕES DE E-MAIL*/
-    define('EMAIL_HOST', 'br518.hostgator.com.br');
+    // define('EMAIL_HOST', 'br518.hostgator.com.br');
+    define('EMAIL_HOST', 'br702.hostgator.com.br');
     define('EMAIL_SMTPAUTH', true);
-    define('EMAIL_USERNAME', 'sistema@pro300.com.br');
-    define('EMAIL_PASSWORD', '$5aK8A~uQv;g');
+    
+    // define('EMAIL_USERNAME', 'sistema@pro300.com.br');
+    // define('EMAIL_PASSWORD', '$5aK8A~uQv;g');
+    
+    define('EMAIL_USERNAME', 'pro300@tarley.eti.br');
+    define('EMAIL_PASSWORD', 'W*Er}VwTD@Eh');
+    
     define('EMAIL_SMTPSECURE', 'ssl');
     define('EMAIL_PORT', 465);
     
     /* CONFIGURAÇÕES LOGENTRIES */
-	//$LOGENTRIES_TOKEN = "bbde0841-f017-41ab-851f-563e6dffb70b";
-    $LOGENTRIES_TOKEN = "15adbbcc-0343-44ea-aba5-411b83f6e437";
+    // $LOGENTRIES_TOKEN = "bbde0841-f017-41ab-851f-563e6dffb70b";
+    // $LOGENTRIES_TOKEN = "15adbbcc-0343-44ea-aba5-411b83f6e437";
+    $LOGENTRIES_TOKEN = "d876b843-7f81-4e87-b641-923fe5814b1b";
     
     // Caminho para a raiz
     define( 'ABSPATH', dirname( __FILE__ ) ); 
